2d49b55ecca0bfb66c5677652635f61b9212e6d2
Abdul Hakeem Shaik <50977399+abdulhakeem143@users.noreply.github.com>
2023-02-21 19:49:30 +0530

    my 2nd commit

--- JavaChallenge1.java
+++ JavaChallenge1.java
@@ -1,5 +1,5 @@
 class JavaChallenge1{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world! This is Hakeem...");
     }
 }
